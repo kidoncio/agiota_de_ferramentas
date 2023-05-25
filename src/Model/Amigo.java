387ebd785bd32ddec11637b8a217e89e9c6463c1
Kaique-Richard-Hames <hameskaique2@gmail.com>
2023-05-25 20:44:18 -0300

    Adiciona classe Amigo

---
+++ src/Model/Amigo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author kaique
+ */
+public class Amigo {
+    private String nome;
+    private String email;
+    private String telefone;
+    
+    public Amigo() {
+        
+    }
+}
